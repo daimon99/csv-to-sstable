f31874220736137f9dc069198a4063884ddd0e77
Daimon@imac <daijian1@qq.com>
2018-08-19 12:27:22 +0800

    only for idcard

---
+++ src/main/java/com/spbtv/cassandra/bulkload/Test.java
@@ -0,0 +1,12 @@
+package com.spbtv.cassandra.bulkload;
+
+/**
+ * Created by daimon on 2018/8/19.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        int result = Integer.parseUnsignedInt("ffffffff", 16);
+        System.out.println(result);
+    }
+}
